17a346d249fca27e21aff693e602e47a811c2673
rlpconsultingsac-ship-it <rlpconsultingsac@gmail.com>
2026-06-24 11:13:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -748,7 +748,7 @@ elif menu == "📊 Estados de Cuenta":
         pdf = FPDF()
         pdf.add_page()
         
-        try: pdf.image("logo.png", x=20, y=20, w=80)
+        try: pdf.image("logo.png", x=15, y=20, w=70)
         except: pass
         
         pdf.set_font("Helvetica", "B", 12)
